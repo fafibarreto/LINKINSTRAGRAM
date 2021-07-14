982f04ebb7d96900b4b2f40e0591ca6e2766f727
fafibarreto <fatimacbeventos@gmail.com>
2021-07-14 19:59:32 +0100

    update tags css/ javascript/
    update tags
    css: all tags are working properly
    javascript:active

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict'
+const switcher=document.querySelector('.btn');
+switcher.addEventListener('click',function(){document.body.classList.toggle('dark-theme')
+
+var className=document.body.className;
+if(className=="light-theme"){ this.textcontent="Dark"}
+
+else{
+    this.textcontent="light;"
+}
+
+});
