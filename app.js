1ecade68cb80c402eda180d808950099514848a5
fafibarreto <fatimacbeventos@gmail.com>
2021-10-20 18:24:35 +0100

    comment
    java.pp

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict'
+/*'use strict'
 const switcher=document.querySelector('.btn');
 switcher.addEventListener('click',function(){document.body.classList.toggle('dark-theme')
 
@@ -9,4 +9,4 @@ else{
     this.textcontent="light;"
 }
 
-});
+});*/
